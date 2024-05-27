70c90856aded65c8ab19f2aea7b290332a271807
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-27 18:21:56 +0700

    PhotoRetrievalException

--- src/main/java/com/vn/htl/back_end/exception/PhotoRetrievalException.java
+++ src/main/java/com/vn/htl/back_end/exception/PhotoRetrievalException.java
@@ -1,4 +1,8 @@
 package com.vn.htl.back_end.exception;
 
-public class PhotoRetrievalException {
+
+public class PhotoRetrievalException extends RuntimeException {
+    public PhotoRetrievalException(String message) {
+        super(message);
+    }
 }
