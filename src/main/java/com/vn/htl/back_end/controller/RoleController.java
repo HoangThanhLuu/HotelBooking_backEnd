7f32cb995eb0efcff6abdceeb21aff0c29e472ab
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-31 00:27:27 +0700

    RoleController PostMapping createRole

--- src/main/java/com/vn/htl/back_end/controller/RoleController.java
+++ src/main/java/com/vn/htl/back_end/controller/RoleController.java
@@ -1,12 +1,12 @@
 package com.vn.htl.back_end.controller;
 
+import com.vn.htl.back_end.exception.RoleAlreadyExistException;
 import com.vn.htl.back_end.model.Role;
 import com.vn.htl.back_end.service.IRoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +20,15 @@ public class RoleController {
     @GetMapping("/all-roles")
     public ResponseEntity<List<Role>> getAllRoles(){
         return new ResponseEntity<>(roleService.getRoles(), FOUND);
+    }
+    @PostMapping("/create-new-role")
+    public ResponseEntity<String> createRole(@RequestBody Role theRole){
+        try{
+            roleService.createRole(theRole);
+            return ResponseEntity.ok("New role created successfully!");
+        }catch(RoleAlreadyExistException re){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(re.getMessage());
+
+        }
     }
 }
