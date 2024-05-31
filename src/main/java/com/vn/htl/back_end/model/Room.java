697ae820416c305411a15c7744355327c981597e
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-31 16:24:53 +0700

    update model xoa cac thong tin k can thiet

--- src/main/java/com/vn/htl/back_end/model/Room.java
+++ src/main/java/com/vn/htl/back_end/model/Room.java
@@ -1,6 +1,5 @@
 package com.vn.htl.back_end.model;
 
-import ch.qos.logback.core.testUtil.RandomUtil;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,30 +11,30 @@ import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
+
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
-@Builder
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
+    private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked = false;
     @Lob
     private Blob photo;
 
-    @OneToMany(mappedBy="room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room() {
         this.bookings = new ArrayList<>();
     }
 
-    public void addBooking(BookedRoom booking){
-        if(bookings == null){
+    public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
             bookings = new ArrayList<>();
         }
         bookings.add(booking);
@@ -44,6 +43,4 @@ public class Room {
         String bookingCode = RandomStringUtils.randomNumeric(10);
         booking.setBookingConfirmationCode(bookingCode);
     }
-
-
 }
