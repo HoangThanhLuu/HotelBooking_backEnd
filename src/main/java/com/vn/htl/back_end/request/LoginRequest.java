b1c342e9984948b91c7326aa22c7a9adbb09b814
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-31 14:16:55 +0700

    LoginRequest

---
+++ src/main/java/com/vn/htl/back_end/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.vn.htl.back_end.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+}
--- src/main/java/com/vn/htl/back_end/security/hotel.java
+++
@@ -1,4 +0,0 @@
-package com.vn.htl.back_end.security;
-
-public class hotel {
-}
