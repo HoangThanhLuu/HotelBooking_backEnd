4bd4f5787dda6692dd7ec5c8b52bad3ce8ee9754
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-31 14:28:30 +0700

    thêm quyền xác thực PreAuthorize cho bookingController

--- src/main/java/com/vn/htl/back_end/controller/BookingController.java
+++ src/main/java/com/vn/htl/back_end/controller/BookingController.java
@@ -8,10 +8,12 @@ import com.vn.htl.back_end.model.Room;
 import com.vn.htl.back_end.response.BookingResponse;
 import com.vn.htl.back_end.response.RoomResponse;
 import com.vn.htl.back_end.service.IBookingService;
+import com.vn.htl.back_end.service.IRoomService;
 import com.vn.htl.back_end.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -22,9 +24,10 @@ import java.util.List;
 @RequestMapping("/bookings")
 public class BookingController {
     private final IBookingService bookingService;
-    private final RoomService roomService;
+    private final IRoomService roomService;
 
     @GetMapping("/all-bookings")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<BookingResponse>> getAllBookings(){
         List<BookedRoom> bookings = bookingService.getAllBookings();
         List<BookingResponse> bookingResponses = new ArrayList<>();
@@ -47,6 +50,7 @@ public class BookingController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
     @GetMapping("/confirmation/{confirmationCode}")
     public ResponseEntity<?> getBookingByConfirmationCode(@PathVariable String confirmationCode){
         try{
@@ -57,10 +61,23 @@ public class BookingController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
         }
     }
+
+    @GetMapping("/user/{email}/bookings")
+    public ResponseEntity<List<BookingResponse>> getBookingsByUserEmail(@PathVariable String email) {
+        List<BookedRoom> bookings = bookingService.getBookingsByUserEmail(email);
+        List<BookingResponse> bookingResponses = new ArrayList<>();
+        for (BookedRoom booking : bookings) {
+            BookingResponse bookingResponse = getBookingResponse(booking);
+            bookingResponses.add(bookingResponse);
+        }
+        return ResponseEntity.ok(bookingResponses);
+    }
+
     @DeleteMapping("/booking/{bookingId}/delete")
     public void cancelBooking(@PathVariable Long bookingId){
         bookingService.cancelBooking(bookingId);
     }
+
     private BookingResponse getBookingResponse(BookedRoom booking) {
         Room theRoom = roomService.getRoomById(booking.getRoom().getId()).get();
         RoomResponse room = new RoomResponse(
@@ -74,5 +91,4 @@ public class BookingController {
                 booking.getNumOfChildren(), booking.getTotalNumOfGuest(),
                 booking.getBookingConfirmationCode(), room);
     }
-
 }
