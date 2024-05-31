d6c04388a22bc38934a23483d5c594c97faa8c25
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-31 14:27:16 +0700

    thêm RoleController

--- src/main/java/com/vn/htl/back_end/controller/RoleController.java
+++ src/main/java/com/vn/htl/back_end/controller/RoleController.java
@@ -18,10 +18,12 @@ import static org.springframework.http.HttpStatus.FOUND;
 @RequiredArgsConstructor
 public class RoleController {
     private final IRoleService roleService;
+
     @GetMapping("/all-roles")
     public ResponseEntity<List<Role>> getAllRoles(){
         return new ResponseEntity<>(roleService.getRoles(), FOUND);
     }
+
     @PostMapping("/create-new-role")
     public ResponseEntity<String> createRole(@RequestBody Role theRole){
         try{
@@ -35,6 +37,17 @@ public class RoleController {
     @DeleteMapping("/delete/{roleId}")
     public void deleteRole(@PathVariable("roleId") Long roleId){
         roleService.deleteRole(roleId);
+    }
+    @PostMapping("/remove-all-users-from-role/{roleId}")
+    public Role removeAllUsersFromRole(@PathVariable("roleId") Long roleId){
+        return roleService.removeAllUsersFromRole(roleId);
+    }
+
+    @PostMapping("/remove-user-from-role")
+    public User removeUserFromRole(
+            @RequestParam("userId") Long userId,
+            @RequestParam("roleId") Long roleId){
+        return roleService.removeUserFromRole(userId, roleId);
     }
     @PostMapping("/assign-user-to-role")
     public User assignUserToRole(
